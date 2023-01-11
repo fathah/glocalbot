26d9fbc9ee67f2a71d5cee68c93dd47d94ed0273
Fathah Cr <abdulfathahkodag@gmail.com>
2023-01-11 07:14:28 +0530

    Nueralnet

---
+++ main.py
@@ -0,0 +1,43 @@
+import pyttsx3
+import speech_recognition
+from neuralintents.main import GenericAssistant
+
+speaker = pyttsx3.init()
+speaker.setProperty('rate', 110)
+voices = speaker.getProperty('voices')
+speaker.setProperty('voice', voices[0].id)
+
+recognizer = speech_recognition.Recognizer()
+
+def event_results(response):
+    speaker.say('Here are the results of the event.')
+    speaker.runAndWait()
+
+def greet():
+    speaker.say('Hello, I am Glocal Bot. What can I do for you?')
+    speaker.runAndWait()
+
+mappings = {
+    "greetings":greet,
+    "event_results":event_results }
+
+glocalbot = GenericAssistant('intents.json',intent_methods= mappings, model_name="test_model")
+glocalbot.load_model()
+
+while True:
+    try:
+        with speech_recognition.Microphone() as mic:
+             recognizer.adjust_for_ambient_noise(mic, duration=0.2) 
+             audio = recognizer.listen(mic,phrase_time_limit=5)
+             text = recognizer.recognize_google(audio)
+             text =  text.lower()
+
+             glocalbot.request(text)
+             
+    except speech_recognition.UnknownValueError:
+        recognizer = speech_recognition.Recognizer()
+        # speaker.say('Sorry, I did not get that.')
+        # speaker.runAndWait()
+        continue
+
+
--- speech.py
+++ speech.py
@@ -1,9 +1,10 @@
 import pyttsx3
 import speech_recognition
+from neuralintents import GenericAssistant
 
 engine = pyttsx3.init()
 
-engine.setProperty('rate', 150)
+engine.setProperty('rate', 100)
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[0].id)
 
@@ -15,12 +16,14 @@ while True:
     try:
         with speech_recognition.Microphone() as mic:
              recognizer.adjust_for_ambient_noise(mic, duration=0.2) 
-             audio = recognizer.listen(mic)
+             audio = recognizer.listen(mic,phrase_time_limit=5)
              text = recognizer.recognize_google(audio)
              text =  text.lower()
 
              print(f"Recognized {text}") 
              engine.say(text)
+             engine.runAndWait()
+             
     except speech_recognition.UnknownValueError:
         recognizer = speech_recognition.Recognizer()
         continue
