7ef038f843b4901a8c34c60f8661da47389c4908
Fathah Cr <abdulfathahkodag@gmail.com>
2023-01-12 16:37:33 +0530

    new response

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 import pyttsx3
 import speech_recognition
 from result_fetch import get_result,read_json
-# from scan import getStudentInfo, scanQr
+from scan import getStudentInfo, scanQr
 from utils import getCampusName, getProgramName, getStudentName
 
 speaker = pyttsx3.init()
@@ -22,7 +22,8 @@ def respond(response):
 
 
 def tell_result(text):
-    response = text.replace('result','').replace('of','')
+    response = text.replace('result','').replace('of','').replace('what','').replace('the','')
+    
     programs = read_json("programs.json")
     for program in programs['data']:
         if  program['name'].lower().replace(' ','') == response.lower().replace(' ',''):
@@ -58,6 +59,9 @@ mappings = {
     "introduce":respond,
     "markaz_garden":respond,
     "thanks":respond,
+    "whatsup":respond,
+    "haha":respond,
+    "programmer":respond,
     }
 
 
@@ -71,11 +75,13 @@ def talkFromIntents(text):
         # else:
         #     speaker.say(text)
         #     speaker.runAndWait()
-        
+
+speaker.say('Hello, I am  Glocal Bot. How can I assist you?')
+speaker.runAndWait()
+
 while True:
     try:
         with speech_recognition.Microphone() as mic:
-             print("started")
              recognizer.adjust_for_ambient_noise(mic, duration=0.2) 
              audio = recognizer.listen(mic,phrase_time_limit=5)
              text = recognizer.recognize_google(audio)
@@ -83,13 +89,13 @@ while True:
 
              if "result" in text or "results" in text:
                 tell_result(text)
-            #  elif "scan" in text:
-            #     speaker.say("Please show your Jamia ID")
-            #     speaker.runAndWait()
-            #     jmId = scanQr()
-            #     if "JM" in jmId:
-            #         student  = getStudentInfo(jmId)
-            #         speaker.say(f"Hello {student['name']} from {getCampusName(student['campus'])}")
+             elif "scan" in text or "identify" in text:
+                speaker.say("Please show your Jamia ID")
+                speaker.runAndWait()
+                jmId = scanQr()
+                if "JM" in jmId:
+                    student  = getStudentInfo(jmId)
+                    speaker.say(f"Hello {student['name']} from {getCampusName(student['campus'])}")
 
              else:
                 talkFromIntents(text)
